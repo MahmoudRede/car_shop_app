bbd5cd095c96d00d857e9692f057b2c8078bcb6f
DELL <mahmoud180280@fci.bu.edu.eg>
2024-02-04 14:43:54 +0200

    search screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:shop_car/business_logic/app_cubit/app_cubit.dart';
 import 'package:shop_car/business_logic/app_cubit/app_states.dart';
 import 'package:shop_car/presentation/screens/home_layout/home_layout.dart';
+import 'package:shop_car/presentation/screens/notifications_screen/notifications_screen.dart';
 import 'package:shop_car/styles/theme_manager/theme_manager.dart';
 
 void main() {
